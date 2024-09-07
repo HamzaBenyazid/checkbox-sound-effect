d3ac2dc4f748d459585f31402b658527934e8c03
HamzaBenyazid <hamzabyazid@gmail.com>
2024-09-07 13:58:58 +0100

    add a setting to customize audio with URL

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
-import { Plugin } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+interface CheckBoxSoundEffectSettings {
+	soundEffectUrl: string;
+}
+
+const DEFAULT_SETTINGS: CheckBoxSoundEffectSettings = {
+	soundEffectUrl: 'https://cdn.pixabay.com/download/audio/2021/08/04/audio_0625c1539c.mp3'
+}
+
 
 export default class CheckBoxSoundEffect extends Plugin {
+	settings: CheckBoxSoundEffectSettings;
+
 	addClassNameListener(elem: HTMLElement, callback: () => void): void {
 		let lastClassName = elem.className;
 		window.setInterval(() => {   
@@ -13,17 +24,53 @@ export default class CheckBoxSoundEffect extends Plugin {
 	}
 	
 	async onload() {
+		await this.loadSettings();
 		this.registerMarkdownPostProcessor((element, _) => {
             const checkboxes = element.querySelectorAll('.task-list-item');
             checkboxes.forEach(checkbox => {
 				const inputElement = checkbox as HTMLInputElement;
                 this.addClassNameListener(inputElement, () => {
                     if (inputElement.classList.contains('is-checked')) {
-                        const audio = new Audio('https://cdn.pixabay.com/download/audio/2021/08/04/audio_0625c1539c.mp3');
+                        const audio = new Audio(this.settings.soundEffectUrl);
                         audio.play();
                     }
                 });
             });
         });
+		this.addSettingTab(new CheckBoxSoundEffectSettingTab(this.app, this));	
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class CheckBoxSoundEffectSettingTab extends PluginSettingTab {
+	plugin: CheckBoxSoundEffect;
+
+	constructor(app: App, plugin: CheckBoxSoundEffect) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Sound Effect URL')
+			.setDesc("web URL for an audio file that you want to use as sound effect when checking a checkbox.")
+			.addText(text => text
+				.setPlaceholder('URL')
+				.setValue(this.plugin.settings.soundEffectUrl)
+				.onChange(async (value) => {
+					this.plugin.settings.soundEffectUrl = value;
+					await this.plugin.saveSettings();
+				}));
 	}
 }
